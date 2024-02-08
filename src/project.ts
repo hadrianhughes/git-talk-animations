c4ef2ccba5f48a64472c3f9baa607bcca6f6b5ec
Hadrian Hughes <hadrianhughes@gmail.com>
2024-02-08 12:12:45 +0000

    Create another scene for fixup

--- src/project.ts
+++ src/project.ts
@@ -1,7 +1,8 @@
 import {makeProject} from '@motion-canvas/core';
 
 import reorder from './scenes/reorder?scene';
+import fixup from './scenes/fixup?scene';
 
 export default makeProject({
-  scenes: [reorder],
+  scenes: [reorder, fixup],
 });
---
+++ src/scenes/fixup.tsx
@@ -0,0 +1,18 @@
+import { makeScene2D } from '@motion-canvas/2d';
+import { createRef } from '@motion-canvas/core';
+import { Terminal } from '../components/Terminal';
+import { GitLine } from '../components/GitLine';
+
+export default makeScene2D(function* (view) {
+  const line1 = createRef<GitLine>();
+  const line2 = createRef<GitLine>();
+  const line3 = createRef<GitLine>();
+
+  view.add(
+    <Terminal width={320} height={120}>
+      <GitLine operation="pick" operand="1fa2ddf3 new feature" ref={line1} />
+      <GitLine operation="fixup" operand="32ac1bef more changes" ref={line2} />
+      <GitLine operation="pick" operand="d8ccf134 debug" ref={line3} />
+    </Terminal>
+  );
+});
