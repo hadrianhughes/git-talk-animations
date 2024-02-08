e9ee24bf5a73f46eb3e50ffb46ed05db6310d1c3
Hadrian Hughes <hadrianhughes@gmail.com>
2024-02-08 13:05:31 +0000

    Create animation where pick command is changed to fixup

--- src/scenes/fixup.tsx
+++ src/scenes/fixup.tsx
@@ -1,5 +1,5 @@
 import { makeScene2D } from '@motion-canvas/2d';
-import { createRef } from '@motion-canvas/core';
+import { createRef, waitFor } from '@motion-canvas/core';
 import { Terminal } from '../components/Terminal';
 import { GitLine } from '../components/GitLine';
 
@@ -11,8 +11,12 @@ export default makeScene2D(function* (view) {
   view.add(
     <Terminal width={320} height={120}>
       <GitLine operation="pick" operand="1fa2ddf3 new feature" ref={line1} />
-      <GitLine operation="fixup" operand="32ac1bef more changes" ref={line2} />
+      <GitLine operation="pick" operand="32ac1bef more changes" ref={line2} />
       <GitLine operation="pick" operand="d8ccf134 debug" ref={line3} />
     </Terminal>
   );
+
+  yield* waitFor(0.5);
+  yield* line2().changeOperation('fixup');
+  yield* waitFor(0.5);
 });
